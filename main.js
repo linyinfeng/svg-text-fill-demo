8742488cb6ca7c3925e4a4cdc3099d83070e1127
Lin Yinfeng <lin.yinfeng@outlook.com>
2023-10-27 12:09:26 +0800

    Initial commit

---
+++ main.js
@@ -0,0 +1,28 @@
+function bind(input, size, preview) {
+    update(size, preview, input.value)
+    input.addEventListener('input', (event) => update(size, preview, event.target.value));
+}
+
+function update(size, preview, text) {
+    preview.textContent = text;
+    bBox = preview.getBBox();
+    size.textContent = `w: ${bBox.width}; h: ${bBox.height}; ctl: ${preview.getComputedTextLength()}`
+}
+
+let input1 = document.getElementById("input-1");
+let input2 = document.getElementById("input-2");
+let input3 = document.getElementById("input-3");
+let size1 = document.getElementById("size-1");
+let size2 = document.getElementById("size-2");
+let size3 = document.getElementById("size-3");
+
+let previewObj = document.getElementById("preview");
+previewObj.addEventListener('load', (event) => {
+    let svgDocument = previewObj.contentDocument;
+    let preview1 = svgDocument.getElementById("tspan389");
+    let preview2 = svgDocument.getElementById("tspan223");
+    let preview3 = svgDocument.getElementById("tspan223-6");
+    bind(input1, size1, preview1);
+    bind(input2, size2, preview2);
+    bind(input3, size3, preview3);
+})
